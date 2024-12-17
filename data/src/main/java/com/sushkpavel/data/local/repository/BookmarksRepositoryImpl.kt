6bc2446475bd2c14eec51f41de8267205e426293
Pavel Sushko <nintsao@gmail.com>
2024-12-17 16:38:43 +0300

    completed data layer at least test for each of these layers (data, domain)

--- data/src/main/java/com/sushkpavel/data/local/repository/BookmarksRepositoryImpl.kt
+++ data/src/main/java/com/sushkpavel/data/local/repository/BookmarksRepositoryImpl.kt
@@ -1,20 +1,44 @@
 package com.sushkpavel.data.local.repository
 
+import com.sushkpavel.data.local.dao.NewsDao
+import com.sushkpavel.data.local.entity.toEntity
+import com.sushkpavel.data.local.entity.toNews
 import com.sushkpavel.domain.model.News
 import com.sushkpavel.domain.repository.BookmarksRepository
 import io.ktor.client.HttpClient
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 
-class BookmarksRepositoryImpl(client: HttpClient) : BookmarksRepository {
-    override suspend fun addBookmark(news: News): Boolean {
-        TODO("Not yet implemented")
+class BookmarksRepositoryImpl(private val newsDao: NewsDao) : BookmarksRepository {
+    override suspend fun addBookmark(news: News): Flow<Boolean> = flow {
+        try {
+            newsDao.insert(news.toEntity())
+            emit(true)
+        } catch (e: Exception) {
+            emit(false)
+        }
     }
 
-    override suspend fun deleteBookmark(news: News): Boolean {
-        TODO("Not yet implemented")
+    override suspend fun deleteBookmark(news: News): Flow<Boolean> = flow {
+        try {
+            newsDao.delete(news.toEntity())
+            emit(true)
+        } catch (e: Exception) {
+            emit(false)
+        }
     }
+
 
     override suspend fun getBookmarks(): Flow<List<News>> {
-        TODO("Not yet implemented")
+        return newsDao.getAll().map{ entities ->
+            entities.map {
+                it.toNews()
+            }
+        }
+    }
+
+    override suspend fun isSaved(news: News): Flow<Boolean> {
+        return newsDao.isSaved(news.url)
     }
 }
